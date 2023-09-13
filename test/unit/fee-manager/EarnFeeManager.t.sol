01e2ac160ec8dbb48503eb4fb08bedf896b8034a
0xKoaj <koaj@balmy.xyz>
2023-09-13 14:17:18 -0300

    test: added more tests

--- test/unit/fee-manager/EarnFeeManager.t.sol
+++ test/unit/fee-manager/EarnFeeManager.t.sol
@@ -13,7 +13,7 @@ contract EarnFeeManagerTest is PRBTest {
   address private manageFeeAdmin = address(2);
   uint16 private defaultPerformanceFee = 300;
   EarnFeeManager private feeManager;
-  StrategyId strategyId;
+  StrategyId aStrategyId;
 
   function setUp() public virtual {
     feeManager = new EarnFeeManager(
@@ -29,6 +29,11 @@ contract EarnFeeManagerTest is PRBTest {
 
   function test_constructor() public {
     assertTrue(feeManager.hasRole(feeManager.MANAGE_FEES_ROLE(), manageFeeAdmin));
+
+    // Access control
+    assertEq(feeManager.defaultAdminDelay(), 3 days);
+    assertEq(feeManager.owner(), superAdmin);
+    assertEq(feeManager.defaultAdmin(), superAdmin);
   }
 
   function test_defaultPerformanceFee() public {
@@ -37,11 +42,54 @@ contract EarnFeeManagerTest is PRBTest {
 
   function test_setDefaultPerformanceFeeWithRole() public {
     assertEq(feeManager.defaultPerformanceFee(), defaultPerformanceFee);
-    vm.startPrank(manageFeeAdmin);
     uint16 newDefaultPerformanceFee = 5;
+
+    vm.startPrank(manageFeeAdmin);
     feeManager.setDefaultPerformanceFee(newDefaultPerformanceFee);
     assertNotEq(feeManager.defaultPerformanceFee(), defaultPerformanceFee);
-    assertEq(feeManager.defaultPerformanceFee(), newDefaultPerformanceFee);
     vm.stopPrank();
+
+    assertEq(feeManager.defaultPerformanceFee(), newDefaultPerformanceFee);
+  }
+
+  function test_modifyPerformanceFeeAndSetBackWithRole() public {
+    assertEq(feeManager.getPerformanceFeeForStrategy(aStrategyId), defaultPerformanceFee);
+    uint16 specificPerformanceFee = 400;
+
+    vm.startPrank(manageFeeAdmin);
+    feeManager.specifyPerformanceFeeForStrategy(aStrategyId, specificPerformanceFee);
+    assertEq(feeManager.getPerformanceFeeForStrategy(aStrategyId), specificPerformanceFee);
+    feeManager.setPerformanceFeeForStrategyBackToDefault(aStrategyId);
+    assertNotEq(feeManager.getPerformanceFeeForStrategy(aStrategyId), specificPerformanceFee);
+    vm.stopPrank();
+
+    assertEq(feeManager.getPerformanceFeeForStrategy(aStrategyId), defaultPerformanceFee);
+  }
+
+  function test_modifyDefaultPerformanceFee_RevertWhenCalledWithoutRole() public {
+    vm.expectRevert(
+      abi.encodeWithSelector(
+        IAccessControl.AccessControlUnauthorizedAccount.selector, address(this), feeManager.MANAGE_FEES_ROLE()
+      )
+    );
+    feeManager.setDefaultPerformanceFee(200);
+  }
+
+  function test_modifyPerformanceFee_RevertWhenCalledWithoutRole() public {
+    vm.expectRevert(
+      abi.encodeWithSelector(
+        IAccessControl.AccessControlUnauthorizedAccount.selector, address(this), feeManager.MANAGE_FEES_ROLE()
+      )
+    );
+    feeManager.specifyPerformanceFeeForStrategy(aStrategyId, 200);
+  }
+
+  function test_setPerformanceFeeBackToDefault_RevertWhenCalledWithoutRole() public {
+    vm.expectRevert(
+      abi.encodeWithSelector(
+        IAccessControl.AccessControlUnauthorizedAccount.selector, address(this), feeManager.MANAGE_FEES_ROLE()
+      )
+    );
+    feeManager.setPerformanceFeeForStrategyBackToDefault(aStrategyId);
   }
 }
