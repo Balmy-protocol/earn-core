e0b06b766d380d2d71ae75b06dfcd933bf883001
0xKoaj <koaj@balmy.xyz>
2023-09-18 16:01:44 -0300

    test: added getStrategy and registerStrategy tests

---
+++ test/unit/strategy-registry/EarnStrategyRegistry.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+// solhint-disable no-unused-import
+import { PRBTest } from "@prb/test/PRBTest.sol";
+import { EarnStrategyRegistry, IEarnStrategyRegistry } from "../../../src/strategy-registry/EarnStrategyRegistry.sol";
+import { Utils } from "../../Utils.sol";
+import { StrategyId } from "../../../src/types/StrategyId.sol";
+
+import { IEarnStrategy } from "../../../src/interfaces/IEarnStrategy.sol";
+
+contract EarnStrategyRegistryTest is PRBTest {
+  IEarnStrategyRegistry strategyRegistry;
+  StrategyId invalidStrategyId = StrategyId.wrap(1000);
+  StrategyId anotherInvalidStrategyId = StrategyId.wrap(1001);
+  address owner = address(1);
+  IEarnStrategy aStrategy;
+
+  function setUp() public virtual {
+    strategyRegistry = new EarnStrategyRegistry();
+  }
+
+  function test_getStrategy_ShouldReturnZero_WhenNonExistentStrategyId() public {
+    assertEq(address(strategyRegistry.getStrategy(invalidStrategyId)), address(0));
+  }
+
+  function test_registerStrategy() public {
+    StrategyId aRegisteredStrategy = strategyRegistry.registerStrategy(owner, aStrategy);
+    assertEq(address(strategyRegistry.getStrategy(aRegisteredStrategy)), address(aStrategy));
+    assertGt(StrategyId.unwrap(aRegisteredStrategy), 0);
+  }
+}
