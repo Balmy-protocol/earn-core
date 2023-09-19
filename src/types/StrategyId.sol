b0178c7578b072f31a219da92a9341d86db02e81
0xKoaj <koaj@balmy.xyz>
2023-09-19 10:54:33 -0300

    refactor: added todo's, lints, strategyId constant and converted function into mappings

--- src/types/StrategyId.sol
+++ src/types/StrategyId.sol
@@ -2,3 +2,13 @@
 pragma solidity >=0.8.0;
 
 type StrategyId is uint96;
+
+using { increment } for StrategyId global;
+
+function increment(StrategyId id) pure returns (StrategyId) {
+  return StrategyId.wrap(StrategyId.unwrap(id) + 1);
+}
+
+library StrategyIdConstants {
+  StrategyId internal constant INITIAL_STRATEGY_ID = StrategyId.wrap(1);
+}
