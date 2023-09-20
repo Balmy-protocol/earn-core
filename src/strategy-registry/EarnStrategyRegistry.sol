b6da9a39b17424ae6ea5cd414a3cfde9f434ebed
0xKoaj <koaj@balmy.xyz>
2023-09-20 17:19:30 -0300

    feat: some lints, mocks and tests

--- src/strategy-registry/EarnStrategyRegistry.sol
+++ src/strategy-registry/EarnStrategyRegistry.sol
@@ -37,7 +37,7 @@ contract EarnStrategyRegistry is IEarnStrategyRegistry {
   function registerStrategy(address firstOwner, IEarnStrategy strategy) external returns (StrategyId strategyId) {
     //_revertIfNotStrategy(address(strategy));
     _revertIfNotAssetAsFirstToken(strategy);
-    if(!(assignedId[strategy] == StrategyId.wrap(0))) revert StrategyAlreadyRegistered();
+    if (!(assignedId[strategy] == StrategyId.wrap(0))) revert StrategyAlreadyRegistered();
     strategyId = _nextStrategyId;
     assignedId[strategy] = strategyId;
     getStrategy[strategyId] = strategy;
@@ -69,7 +69,7 @@ contract EarnStrategyRegistry is IEarnStrategyRegistry {
   }
 
   function _revertIfNotAssetAsFirstToken(IEarnStrategy _strategyToCheck) internal view {
-   (address[] memory tokens, )  = _strategyToCheck.allTokens();
+    (address[] memory tokens,) = _strategyToCheck.allTokens();
     bool _isAssetFirstToken = _strategyToCheck.asset() == tokens[0];
     if (!_isAssetFirstToken) revert AssetIsNotFirstToken(_strategyToCheck);
   }
